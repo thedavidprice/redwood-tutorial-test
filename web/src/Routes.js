624ab63a1d8566562c202f2a194136df38c125ea
David S Price <thedavid@thedavidprice.com>
2020-03-27 20:35:47 -0700

    up through administration

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -14,10 +14,14 @@ const Routes = () => {
     <Router>
       <Route path="/contact" page={ContactPage} name="contact" />
       <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
-      <Route path="/posts/new" page={NewPostPage} name="newPost" />
-      <Route path="/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
-      <Route path="/posts/{id:Int}" page={PostPage} name="post" />
-      <Route path="/posts" page={PostsPage} name="posts" />
+      <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
+      <Route
+        path="/admin/posts/{id:Int}/edit"
+        page={EditPostPage}
+        name="editPost"
+      />
+      <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
+      <Route path="/admin/posts" page={PostsPage} name="posts" />
       <Route path="/about" page={AboutPage} name="about" />
       <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
