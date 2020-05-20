485146b53361095078afe6d6ead1325a5c85b70c
David S Price <thedavid@thedavidprice.com>
2020-05-20 14:33:38 -0700

    add Auth code updates per tutorial

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -7,21 +7,23 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Private } from '@redwoodjs/router'
 
 const Routes = () => {
   return (
     <Router>
       <Route path="/contact" page={ContactPage} name="contact" />
       <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
-      <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
-      <Route
-        path="/admin/posts/{id:Int}/edit"
-        page={EditPostPage}
-        name="editPost"
-      />
-      <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
-      <Route path="/admin/posts" page={PostsPage} name="posts" />
+      <Private unauthenticated="home">
+        <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
+        <Route
+          path="/admin/posts/{id:Int}/edit"
+          page={EditPostPage}
+          name="editPost"
+        />
+        <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
+        <Route path="/admin/posts" page={PostsPage} name="posts" />
+      </Private>
       <Route path="/about" page={AboutPage} name="about" />
       <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
