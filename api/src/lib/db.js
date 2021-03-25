b1ffa9917c66da01c18854f465786653bc42b4da
David S Price <thedavid@thedavidprice.com>
2021-03-24 23:52:04 -0700

    v0.28 code mods

--- api/src/lib/db.js
+++ api/src/lib/db.js
@@ -3,7 +3,22 @@
 
 import { PrismaClient } from '@prisma/client'
 
-export const db = new PrismaClient()
+import { emitLogLevels, handlePrismaLogging } from '@redwoodjs/api/logger'
+
+import { logger } from './logger'
+
+/*
+ * Instance of the Prisma Client
+ */
+export const db = new PrismaClient({
+  log: emitLogLevels(['info', 'warn', 'error']),
+})
+
+handlePrismaLogging({
+  db,
+  logger,
+  logLevels: ['info', 'warn', 'error'],
+})
 
 // Using this as a workaround until Prisma has a chance to work on
 // https://github.com/prisma/prisma/issues/2152
