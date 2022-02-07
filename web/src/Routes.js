636d59f5dea561054227edeb9fc0e69202f6b583
David Price <thedavid@thedavidprice.com>
2022-02-07 10:13:58 -0800

    no prerender

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -17,18 +17,26 @@ const Routes = () => {
       <Set wrap={BlogLayout}>
         <Route path="/contact" page={ContactPage} name="contact" />
         <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
-        <Route path="/about" page={AboutPage} name="about" prerender />
-        <Route path="/" page={HomePage} name="home" prerender />
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" />
       </Set>
       <Private unauthenticated="home">
         <Set wrap={PostsLayout}>
-          <Route path="/admin/posts/new" page={PostNewPostPage} name="newPost" />
-          <Route path="/admin/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+          <Route
+            path="/admin/posts/new"
+            page={PostNewPostPage}
+            name="newPost"
+          />
+          <Route
+            path="/admin/posts/{id:Int}/edit"
+            page={PostEditPostPage}
+            name="editPost"
+          />
           <Route path="/admin/posts/{id:Int}" page={PostPostPage} name="post" />
           <Route path="/admin/posts" page={PostPostsPage} name="posts" />
         </Set>
       </Private>
-      <Route notfound page={NotFoundPage} prerender />
+      <Route notfound page={NotFoundPage} />
     </Router>
   )
 }
